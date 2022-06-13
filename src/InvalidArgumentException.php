fafe362f790f43bba4c110d627073f681286bdaa
TOVOHERY <tovoherypascal@gmail.com>
2022-06-13 03:10:06 +0300

    InvalidArgumentException implementation of the interface

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -1,9 +1,17 @@
 <?php
+namespace Dadapas\Cache;
+
+/**
+ * This file is part of the dadapas/cache library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) TOVOHERY Z. Pascal <tovoherypascal@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
 
 use Exception;
-use Psr\SimpleCache\InvalidArgumentException;
+use Psr\Cache\InvalidArgumentException as InvalidArgumentExceptionIterface;
 
-class InvalidArgumentException extends Exception implements InvalidArgumentException
-{
-
-}
+class InvalidArgumentException extends Exception implements InvalidArgumentExceptionIterface {}
