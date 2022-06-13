90c864f843202d2b5db80fc555f7e537a18fa544
TOVOHERY <tovoherypascal@gmail.com>
2022-06-13 03:13:09 +0300

    Licence to the author

--- src/CacheException.php
+++ src/CacheException.php
@@ -1,10 +1,17 @@
 <?php
 namespace Dadapas\Cache;
 
-use Psr\SimpleCache\CacheException;
+/**
+ * This file is part of the dadapas/cache library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) TOVOHERY Z. Pascal <tovoherypascal@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+use Psr\Cache\CacheException;
 use Exception;
 
-class CacheException extends Exception implements CacheException
-{
-
-}
+class CacheException extends Exception implements CacheException {}
